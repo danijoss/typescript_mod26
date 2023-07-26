c96f8fdf932cbb2529b193f6aefda49607c3a5e3
Daniel <dan.henrri@gmail.com>
2023-07-26 17:08:29 -0300

    submissao exercicio 26

---
+++ exercicio26.ts
@@ -0,0 +1,14 @@
+//exercicio 26 modulo Introdução ao TypeScript
+
+//funçao para multiplicar numeros:
+
+const multiplicaNumeros = (numero1: number, numero2: number): number => numero1 * numero2;
+
+console.log(multiplicaNumeros(4, 6))
+
+
+function funcaoDeSaudacao(nome: string): string {
+        return 'Olá, ' + nome;
+}
+
+console.log(funcaoDeSaudacao('Daniel'))
